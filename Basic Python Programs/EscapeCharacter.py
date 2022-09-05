d2fefbc29eb2f019c12c2416075ec6a7f9c7d473
Paidipelly Dhruvateja <81409709+P-Dhruva-Teja@users.noreply.github.com>
2022-09-05 12:34:48 +0530

    Update EscapeCharacter.py

--- Basic Python Programs/EscapeCharacter.py
+++ Basic Python Programs/EscapeCharacter.py
@@ -1,3 +1,22 @@
-### \n -> New Line
+# \n -> New Line
 suppose = "This string has been \nsplit over \nseveral \nlines"
 print(suppose)
+"""output : This string has been
+split over
+several 
+lines"""
+
+# \t -> tab space
+tab_space = "1\t2\t3\t4\t5"
+print(tab_space)
+#output : 1 2 3 4 5
+
+#\ -> the blackslash is used to escape special characters such as quotes and double quotes
+print("The customer asked, \"Why don\'t you sell books\"?")
+#output : The customer asked, "Why don't you sell books"?
+split_string = "This string has been \
+split over \
+several \
+lines"
+#output : This string has been split over several lines
+
